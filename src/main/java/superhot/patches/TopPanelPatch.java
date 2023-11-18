d7ab70d8d5bf54a4eb9985cb8c6a3821e302e09f
luckytyphlosion <10688458+luckytyphlosion@users.noreply.github.com>
2023-11-17 20:52:50 -0500

    Display milliseconds in run time.

--- src/main/java/superhot/patches/TopPanelPatch.java
+++ src/main/java/superhot/patches/TopPanelPatch.java
@@ -3,17 +3,48 @@ package superhot.patches;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInstrumentPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.screens.stats.CharStat;
 import com.megacrit.cardcrawl.ui.panels.TopPanel;
 
 import javassist.CannotCompileException;
 import javassist.expr.ExprEditor;
 import javassist.expr.FieldAccess;
+import javassist.expr.MethodCall;
 
 @SpirePatch(
     clz = TopPanel.class,
     method = "render"
 )
 public class TopPanelPatch {
+    public static String formatHMSM_withMillis(float t) {
+        String res = "";
+        long duration = (long)t;
+        double seconds = t % 60;
+        duration /= 60L;
+        int minutes = (int)(duration % 60L);
+        int hours = (int)t / 3600;
+
+        if (hours > 0) {
+            res = String.format("%02d:%02d:%.03f", hours, minutes, seconds);
+        } else {
+            res = String.format("%02d:%.03f", minutes, seconds);
+        } 
+        return res;
+    }
+
+    @SpireInstrumentPatch()
+    public static ExprEditor replaceFormatCalls()
+    {
+        return new ExprEditor() {
+            @Override
+            public void edit(MethodCall m) throws CannotCompileException {
+                if (m.getMethodName().equals("formatHMSM") && m.getClassName().equals(CharStat.class.getName())) {
+                    m.replace("$_ = " + TopPanelPatch.class.getName() + ".formatHMSM_withMillis($1);");
+                }
+            }
+        };
+    }
+
     @SpireInstrumentPatch()
     public static ExprEditor modifyCondition()
     {
